fa05bc6c8369f346b808be08aa57fb3c02f37cd2
21f1000520 <21f1000520@ds.study.iitm.ac.in>
2024-12-19 21:26:45 +0530

    bug fixes

--- static/components/Campaign_view.js
+++ static/components/Campaign_view.js
@@ -5,7 +5,7 @@ import UpdateCamp from "./Update_campaign.js";
 const campaign = {
     template: `
     <div  >
-        <div :class="'row row-cols-'+camps.length+'g-3'">
+        <div :class="'row row-cols-'+((camps.length > 3) ? '3' : camps.length)+ ' g-3' " class="d-flex justify-content-center">
             <div v-for="camp in camps" class="col">
                 <div class="card text-dark shadow-lg" v-bind:class="{ 'bg-light': (camp.visibility && !camp.flag), 'bg-warning': (!camp.visibility && !camp.flag) ,'bg-custom':camp.flag}">
                     <div class="card-header"><h5>{{camp.name}}</h5></div>
@@ -21,7 +21,7 @@ const campaign = {
                         <button class="btn btn-info shadow" style="border-radius: 16px;" @click="view_camp(camp.id,camp.name)" ><i class="bi bi-view-list"></i> View</button>
                         
                         <button class="btn btn-primary shadow" style="border-radius: 16px;" @click="update_camp(camp.id)" ><i class="bi bi-cloud-arrow-up"></i> Update</button>
-                        <button class="btn btn-danger shadow" style="border-radius: 16px;" @dbclick="delete_camp(camp.id)" @click="this.alert('double click to delete')"><i class="bi bi-trash"></i> Delete</button>
+                        <button class="btn btn-danger shadow" style="border-radius: 16px;" @dblclick="delete_camp(camp.id)"><i class="bi bi-trash"></i> Delete</button>
                     </div>
 
                     <div class="card-footer alert alert-danger" role="alert" v-else>
@@ -109,7 +109,7 @@ const campaign = {
                 let datas = await res.json();
                 // this.all_influencers = datas;
                 console.log(datas);
-                this.$router.go()
+                this.$emit("reload_campaigns")
             } else {
                 let errorData = await res.json();
                 console.error("Could not delete", errorData);
